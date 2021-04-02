06eae95d4d3f5cc654f4551bbeaf68c0c5fda0b1
KE Rui <kedarui@outlook.com>
2021-04-03 01:05:22 +0800

    init

---
+++ service/db.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+
+uri = '**Confidential**'
+client = MongoClient(uri)
+db = client['2019-nCoV']
+
+
+class DB:
+    def __init__(self):
+        self.db = db
+
+    def insert(self, collection, data):
+        self.db[collection].insert(data)
+
+    def find_one(self, collection, data=None):
+        return self.db[collection].find_one(data)
